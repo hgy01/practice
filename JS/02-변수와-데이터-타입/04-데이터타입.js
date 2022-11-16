abaa28118e348d71365e397257092149df7d3282
hgy01 <hgy7197@gmail.com>
2022-11-17 01:16:28 +0900

    '22.11.16'

---
+++ JS/02-변수와-데이터-타입/04-데이터타입.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//1번
+let sampleValue1=123;
+console.log(typeof sampleValue1);
+
+//2번
+let sampleValue2=true;
+console.log(typeof sampleValue2);
+
+//3번
+let sampleValue3='hello world';
+console.log(typeof sampleValue3);
+
+//4번
+let sampleValue4=new Date();
+console.log(typeof sampleValue4);
+
+//5번
+let sampleValue5=null;
+console.log(typeof sampleValue5);
+
+//6번
+let sampleValue6;
+console.log(typeof sampleValue6);
