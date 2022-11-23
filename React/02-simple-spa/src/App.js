6241fc5db368c51405425dc34c60298ebc747c28
hgy01 <hgy7197@gmail.com>
2022-11-23 23:59:10 +0900

    '22.11.23'

--- React/02-simple-spa/src/App.js
+++ React/02-simple-spa/src/App.js
@@ -4,6 +4,9 @@ import {Link, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Main from './pages/Main';
+import DepartmentGet from './pages/DepartmentGet';
+import DepartmentPath from './pages/DepartmentPath';
+import Error404 from './pages/Error404';
 
 
 const App=()=>{
@@ -16,6 +19,12 @@ const App=()=>{
         <Link to="/">[Home]</Link>
         <Link to="/about">[About]</Link>
         <Link to='/main'>[Main(SubRout)]</Link>
+        {/* http get 파라미터를 포함하는 링크 구성 */}
+        <Link to='/department_get?id=101&msg=hello'>[컴퓨터공학과]</Link>
+        <Link to='/department_get?id=102&msg=world'>[멀티미디어학과]</Link>
+        {/* path 파라미터를 포함하는 링크 구성 */}
+        <Link to='/department_path/201/hello'>[전자공학과]</Link>
+        <Link to='/department_path/202/world'>[기계공학과]</Link>
       </nav>
 
       <a href="/about">일반링크</a>
@@ -27,6 +36,12 @@ const App=()=>{
         <Route path="/about" element={<About/>}/>
         {/* 서브라우팅 사용 */}
         <Route path='/main/*' element={<Main/>}/>
+        {/* GET 파라미터 사용 */}
+        <Route path='/department_get' element={<DepartmentGet/>}/>
+        {/* path파라미터는 url 형식에 변수의 위치와 이름을 정해줘야 한다 */}
+        <Route path='/department_path/:id/:msg' element={<DepartmentPath/>}/>
+        {/* path 속성 없이 route 지정->지정되지 않은 모든 요청에 반응. 단 routes 블록의 맨 마지막에 배치해야 함 */}
+        <Route path='/*' element={<Error404/>}/>
       </Routes>
     </div>
   );
