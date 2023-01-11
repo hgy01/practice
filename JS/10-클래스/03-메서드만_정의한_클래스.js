e192f9f76b10153a47d13b81ee0133a76a6ad652
hgy01 <hgy7197@gmail.com>
2023-01-11 23:10:22 +0900

    '23.01.11'

---
+++ JS/10-클래스/03-메서드만_정의한_클래스.js
@@ -0,0 +1,15 @@
+class Clac
+{
+    plus(x,y)
+    {
+        return x+y;
+    }
+    minus(x,y)
+    {
+        return x-y;
+    }
+}
+const c= new Clac();
+console.log(c);
+console.log(c.plus(1,2));
+console.log(c.minus(10,5));
